9ef6874cd87abe96b677c3fd1d26888da9330d50
Ethan <ihuang22@wisc.edu>
2024-01-11 14:28:59 -0500

    Create sim-mfu.c
    add sim-mfu.c

---
+++ sim-mfu.c
@@ -0,0 +1,100 @@
+//Filename: sim-lru.c
+//Author: Ethan Huang
+//Partner: NONE
+
+
+#include "pt/pagetable.h"
+#define USAGE "USAGE: 537pfsim-lru -p pagesize -m physical_memory tracefile\n"
+int PAGE_SIZE = 0x1000; //default value
+int PHYSICAL_FRAMES = 0x100; // default value
+int VIRTUAL_PAGES = 20; //20
+char *tracefile = NULL;
+
+
+/**********************************************************************
+
+    Function    : main
+    Description : this is the main function for project #4
+    Inputs      : argc - number of command line parameters
+                  argv - the text of the arguments
+    Outputs     : 0 if successful, -1 if failure
+
+***********************************************************************/
+
+//main function
+int main(int argc, char **argv)
+{
+    int opt;
+    unsigned int PHYSICAL_MEMORY = 0x400 * 0x400;
+    //FILE *tfp = stdin;
+    int ret = 0;
+    int MEM;
+    int t = 0;
+    char *foput = "mfuStats.txt";
+    //for mfu
+    char *replacement_alg = "4";
+
+    // /* Check for arguments */
+    if (argc < 4)
+    {
+        /* Complain, explain, and exit */
+        fprintf(stderr, "missing or bad command line arguments\n");
+        fprintf(stderr, USAGE);
+        exit(-1);
+    }
+
+    printf("main in \n");
+    while ((opt = getopt(argc, argv, "m:p:")) != -1)
+    {
+
+        switch (opt)
+        {
+        case 'p':
+            PAGE_SIZE = (int)strtoul(optarg, NULL, 10);
+            if(PAGE_SIZE != 0){
+                t = 1;
+            }else{
+                PAGE_SIZE = 0x1000;
+            }
+
+            break;
+        case 'm':
+            MEM = (int)strtoul(optarg, NULL, 10);
+            if(MEM != 0) t = 1;
+            break;
+        default:
+            fprintf(stderr, "missing or bad command line arguments\n");
+            fprintf(stderr, "%s", USAGE);
+            break;
+        }
+    }
+
+    if (MEM != 0)
+        PHYSICAL_MEMORY = MEM * 0x400 * 0x400;
+    if (t == 1)
+        PHYSICAL_FRAMES = (abs)(PHYSICAL_MEMORY / PAGE_SIZE);
+
+    tracefile = argv[argc-1];
+    printf("tracefile = %s\n", tracefile);
+    printf("Physical Memory = %d, PAGE_SIZE = %d \n", PHYSICAL_MEMORY, PAGE_SIZE);
+
+    //PHYSICAL_FRAMES =  PHYSICAL_MEMORY/ PAGE_SIZE;
+    printf("PHYSICAL_FRAMES = %d\n",  PHYSICAL_FRAMES);
+    
+    //assert by allowance value
+    assert(PAGE_SIZE>0);
+    assert(PAGE_SIZE<PHYSICAL_MEMORY);
+    
+    if (tracefile != NULL)
+    {
+        ret = parse_data(tracefile,foput,replacement_alg);
+    }
+
+    return ret;
+}
+
+
+//change file read order
+// FILE *reorder(FILE inputfile){
+//     return inputfile;
+// }
